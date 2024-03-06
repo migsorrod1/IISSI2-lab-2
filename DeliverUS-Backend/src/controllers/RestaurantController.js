d29b419dda6bbcf5e8d2ca890270872997c7f8e0
Miguel Soriano Rodríguez <migsorrod1@alum.us.es>
2024-03-06 19:09:24 +0100

    practica 2 completada

--- DeliverUS-Backend/src/controllers/RestaurantController.js
+++ DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -2,7 +2,7 @@ import { Restaurant, Product, RestaurantCategory, ProductCategory } from '../mod
 
 const index = async function (req, res) {
   try {
-    const restaurants = await Restaurant.findAll(
+    const restaurants = await Restaurant.findAll(req.params,
       {
         attributes: { exclude: ['userId'] },
         include:
@@ -19,21 +19,70 @@ const index = async function (req, res) {
   }
 }
 
-// TODO: Complete the following functions
-
 const create = async function (req, res) {
+
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = 1
+
+  try {
+  const restaurant = await newRestaurant.save()
+  res.json(restaurant)
+} catch (err) {
+    res.status(500).send(err)
+}
 
 }
 
 const show = async function (req, res) {
-
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+    attributes: { exclude: ['userId'] },
+    include: [{
+      model: Product,
+      as: 'products',
+      include: { model: ProductCategory, as: 'productCategory' }
+    },
+    {
+      model: RestaurantCategory,
+      as: 'restaurantCategory'
+    }],
+    order: [[{model:Product, as: 'products'}, 'order', 'ASC']],
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
+  try {
+    const existingRestaurant = Restaurant.findByPk(req.params.restaurantId)
+    
+    await existingRestaurant.update(req.body)
 
+    const updatedRestaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+    }
+    )
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
+
+  try {
+    await Restaurant.destroy({
+      where: {
+        id: req.params.restaurantId},
+    }
+    )
+    res.json({ message: 'Restaurante eliminado' });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 
 }
 
